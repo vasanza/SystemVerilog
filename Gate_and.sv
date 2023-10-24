90251f85d5179ce06eead81a3afe14ca54da9944
vasanza <vasanza@gmail.com>
2023-10-23 21:48:32 -0500

    Update Gate_and.sv

--- Gate_and.sv
+++ Gate_and.sv
@@ -4,10 +4,12 @@
 // Read more: 
 // https://vasanza.blogspot.com
 
+// Module
 module Gate_and #(parameter n = 1) (
     input logic [n-1:0] A,
     input logic [n-1:0] B,
     output logic [n-1:0] F
 );
+    // Output assignment
     assign F = A & B;
 endmodule
