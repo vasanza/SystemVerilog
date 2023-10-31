863b6eb37579694ea53712ff1390c770d7044e28
vasanza <vasanza@gmail.com>
2023-10-31 17:49:08 -0500

    Add files via upload

---
+++ Add.sv
@@ -0,0 +1,21 @@
+// Add
+// Adder of 2 numbers of n bits
+// Repository: https://github.com/vasanza/SystemVerilog
+// Read more: https://vasanza.blogspot.com
+
+// Package import
+`include "sv_stdlib.sv"
+
+// Module declaration
+module Add #(parameter int n=4) (
+    input logic signed [n-1:0] X, Y,
+    output logic signed [n:0] S
+);
+    // Signals, Constants, Variables, Components
+    
+    // Logic for adding inputs X and Y
+    always_comb begin
+        S = X + Y;
+    end
+
+endmodule
